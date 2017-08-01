ea78058354472f49be59f4a2148069b8b986788d
Miguel Gomez <mgomez@albany.edu>
2017-08-01 14:20:32 -0600

    Add files via upload

---
+++ LinkedListQuestions/DsAlgoQuestion/palin.java
@@ -0,0 +1,73 @@
+
+import java.util.*;
+
+public class palin {
+    public static void main(String[] args) {
+
+        Node head = new Node(9);
+        addNode(head, 8);
+        addNode(head, 7);
+        addNode(head, 6);
+        addNode(head, 5);
+        addNode(head, 4);
+        addNode(head, 3);
+        addNode(head, 2);
+        addNode(head, 1);
+
+        Node newList = reverseList(head);
+
+        while(newList != null){
+            System.out.println(newList.data);
+            newList = newList.next;
+        }
+
+
+
+    }
+
+    public static Node reverseList(Node originalH){
+        Node head = originalH;
+        Node newHead = null;
+
+        while(head != null){
+            Node temp = new Node(head.data);
+
+            temp.next = newHead;
+            newHead = temp;
+
+            head = head.next;
+
+        }
+
+        return newHead;
+
+    }
+
+
+
+    public static void addNode(Node head, int data){
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+
+        temp.next = new Node(data);
+    }
+
+
+
+
+
+}
+
+
+class Node{
+    Node next = null;
+    int data;
+
+    Node(int data){
+        this.data = data;
+        this.next = next;
+    }
+}
+
