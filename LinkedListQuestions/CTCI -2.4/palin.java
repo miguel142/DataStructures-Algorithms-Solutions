c111f60969a6de85522820466741c377ff6d415c
Miguel Gomez <mgomez@albany.edu>
2017-08-01 13:41:18 -0600

    Add files via upload

---
+++ LinkedListQuestions/CTCI -2.4/palin.java
@@ -0,0 +1,86 @@
+
+import java.util.*;
+
+public class palin {
+    public static void main(String[] args) {
+
+        Node head = new Node(5);
+        addNode(head, 4);
+        addNode(head, 3);
+        addNode(head, 2);
+        addNode(head, 1);
+
+        converter(head, 3);
+
+
+
+    }
+
+    public static void converter(Node head, int x){
+
+       Node temp = head;
+       Node front = null;
+       Node back = null;
+        while(temp != null){
+
+            Node current = new Node(temp.data);
+            if(front == null){
+                front = current;
+                back = front;
+            }
+
+            else if(temp.data < x){
+
+                current.next = front;
+                front = current;
+            }
+
+            else{
+                back.next = current;
+                back = current;
+            }
+
+            temp = temp.next;
+        }
+
+        Node newCurrent = front;
+
+        while(newCurrent != null){
+            System.out.println(newCurrent.data);
+            newCurrent = newCurrent.next;
+        }
+
+
+
+    }
+
+
+
+    public static void addNode(Node head, int data){
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+
+        temp.next = new Node(data);
+    }
+
+
+
+
+
+}
+
+
+class Node{
+    Node next = null;
+    int data;
+
+
+
+    Node(int data){
+        this.data = data;
+        this.next = next;
+    }
+}
+
