7c8569fab653744f32915ed29f59c83afee8bd70
Miguel Gomez <mgomez@albany.edu>
2017-08-03 16:51:53 -0400

    Update palin.java

--- Arrays_n_Strings/DsAlgo_6/palin.java
+++ Arrays_n_Strings/DsAlgo_6/palin.java
@@ -4,25 +4,6 @@ import java.util.*;
 public class palin {
     public static void main(String[] args) {
 
-//        Node head = new Node(9);
-//        addNode(head, 8);
-//        addNode(head, 7);
-//        addNode(head, 6);
-//        addNode(head, 5);
-//        addNode(head, 4);
-//        addNode(head, 3);
-//        addNode(head, 2);
-//        addNode(head, 1);
-//
-//        Node newList = lastNode(head, 3);
-//
-//        System.out.println(newList.data);
-
-//        while(newList != null){
-//            System.out.println(newList.data);
-//            newList = newList.next;
-//        }
-
         int[] array = {0, 0, 1, 5, 0, 3};
 
          moveZeros(array);
@@ -74,32 +55,7 @@ public class palin {
 
     //Answer
 
-    //Answer
-
-//
-//    public static void addNode(Node head, int data){
-//        Node temp = head;
-//        while(temp.next != null){
-//            temp = temp.next;
-//        }
-//
-//        temp.next = new Node(data);
-//    }
-
-
-
-
 
 }
 
-
-//class Node{
-//    Node next = null;
-//    int data;
-//
-//    Node(int data){
-//        this.data = data;
-//        this.next = next;
-//    }
-//}
 
