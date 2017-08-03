ef3811ca9179e5a90cbb634f9ce036555b32d54d
Miguel Gomez <mgomez@albany.edu>
2017-08-03 15:45:22 -0400

    Update palin.java

--- Arrays_n_Strings/DsAlgo_1Stack/palin.java
+++ Arrays_n_Strings/DsAlgo_1Stack/palin.java
@@ -3,25 +3,7 @@ import java.util.*;
 
 public class palin {
     public static void main(String[] args) {
-
-//        Node head = new Node(9);
-//        addNode(head, 8);
-//        addNode(head, 7);
-//        addNode(head, 6);
-//        addNode(head, 5);
-//        addNode(head, 4);
-//        addNode(head, 3);
-//        addNode(head, 2);
-//        addNode(head, 1);
-//
-//        Node newList = lastNode(head, 3);
-//
-//        System.out.println(newList.data);
-
-//        while(newList != null){
-//            System.out.println(newList.data);
-//            newList = newList.next;
-//        }
+        
         int[] array = {6, 4, 1, 5, 3, 8};
 
          nextElement(array);
@@ -58,43 +40,12 @@ public class palin {
                 newArray[i] = 0;
             }
 
-
         }
-
-//        for(int i = 0; i < newArray.length; i++){
-//            System.out.println(newArray[i]);
-//        }
 
     }
 
     //Answer
 
-    //Answer
-
-//
-//    public static void addNode(Node head, int data){
-//        Node temp = head;
-//        while(temp.next != null){
-//            temp = temp.next;
-//        }
-//
-//        temp.next = new Node(data);
-//    }
-
-
-
-
-
 }
 
-
-//class Node{
-//    Node next = null;
-//    int data;
-//
-//    Node(int data){
-//        this.data = data;
-//        this.next = next;
-//    }
-//}
 
