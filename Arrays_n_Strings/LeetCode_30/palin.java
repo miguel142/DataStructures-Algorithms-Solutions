2ea47869cb268c107baf07c59ec66a52a37cee2d
Miguel Gomez <mgomez@albany.edu>
2017-08-06 16:01:41 -0400

    Update palin.java

--- Arrays_n_Strings/LeetCode_30/palin.java
+++ Arrays_n_Strings/LeetCode_30/palin.java
@@ -4,25 +4,6 @@ import java.util.*;
 public class palin {
     public static void main(String[] args) {
 
-//        Node head = new Node(9);
-//        addNode(head, 8);
-//        addNode(head, 7);
-//        addNode(head, 6);
-//        addNode(head, 5);
-//        addNode(head, 4);
-//        addNode(head, 3);
-//        addNode(head, 2);
-//        addNode(head, 1);
-//
-//        Node newList = lastNode(head, 3);
-//
-//        System.out.println(newList.data);
-
-//        while(newList != null){
-//            System.out.println(newList.data);
-//            newList = newList.next;
-//        }
-
       String str = "abcd ef gh";
       String[] words = {"ab", "gh"};
 
@@ -65,44 +46,8 @@ public class palin {
 
     //Answer
 
-    //Answer
-
-    //Answer
-
-
-
-
-
-//    public static char[] swap(char[] word, int front, int back){
-//
-//    }
-
-
-
-        //Answer
-
-        //Answer
-
-//
-//    public static void addNode(Node head, int data){
-//        Node temp = head;
-//        while(temp.next != null){
-//            temp = temp.next;
-//        }
-//
-//        temp.next = new Node(data);
-//    }
-
 }
 
 
-//class Node{
-//    Node next = null;
-//    int data;
-//
-//    Node(int data){
-//        this.data = data;
-//        this.next = next;
-//    }
-//}
+
 
