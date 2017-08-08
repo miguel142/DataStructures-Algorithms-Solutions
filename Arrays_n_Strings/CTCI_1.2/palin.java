f6c448d4e7150b969585dd83577770fdfa5b97ff
Miguel Gomez <mgomez@albany.edu>
2017-08-08 09:58:08 -0600

    Add files via upload

---
+++ Arrays_n_Strings/CTCI_1.2/palin.java
@@ -0,0 +1,203 @@
+
+import java.util.*;
+
+public class palin {
+
+    public static void main(String[] args) {
+
+//        Node head = new Node(9);
+//        addNode(head, 8);
+//        addNode(head, 7);
+//        addNode(head, 6);
+//        addNode(head, 5);
+//        addNode(head, 4);
+//        addNode(head, 3);
+//        addNode(head, 2);
+//        addNode(head, 1);
+//
+//        Node newList = lastNode(head, 3);
+//
+//        System.out.println(newList.data);
+
+//        while(newList != null){
+//            System.out.println(newList.data);
+//            newList = newList.next;
+//        }
+//        Node one = new Node(1);
+//        Node two = new Node(2);
+//        Node three = new Node(3);
+//        Node five = new Node(5);
+//
+//        Node head = insert(null, five);
+//
+//        insert(head, one);
+//        insert(head, two);
+//        insert(head, three);
+
+        String str1 = "word";
+        String str2 = "dors";
+
+        boolean list = permutation(str1, str2);
+
+        System.out.println(list);
+
+    }
+
+    //Answer
+
+    public static boolean permutation(String str1, String str2){
+
+        HashSet set = new HashSet();
+
+        char[] string1 = str1.toCharArray();
+        char[] string2 = str2.toCharArray();
+
+        for(int i = 0; i < str1.length(); i++ ){
+            set.add(string1[i]);
+        }
+
+        for(int j = 0; j < str2.length(); j++){
+            if(!set.contains(string2[j])){
+                return false;
+            }
+        }
+
+        return true;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//    public static Node insert(Node head, Node node) {
+//        if (head == null) {
+//            return node;
+//        }
+//
+//        if (node.getData() <= head.getData()) {
+//            head.setLeftChild(insert(head.getLeftChild(), node));
+//        } else {
+//            head.setRightChild(insert(head.getRightChild(), node));
+//        }
+//
+//        return head;
+//    }
+}
+
+
+
+class Node{
+    int data;
+    Node leftChild;
+    Node rightChild;
+
+    Node(int data){
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    public static char[] swap(char[] word, int front, int back){
+//
+//    }
+
+
+
+        //Answer
+
+        //Answer
+
+//
+//    public static void addNode(Node head, int data){
+//        Node temp = head;
+//        while(temp.next != null){
+//            temp = temp.next;
+//        }
+//
+//        temp.next = new Node(data);
+//    }
+
+
+
+
+//class Node{
+//    Node next = null;
+//    int data;
+//
+//    Node(int data){
+//        this.data = data;
+//        this.next = next;
+//    }
+//}
+
