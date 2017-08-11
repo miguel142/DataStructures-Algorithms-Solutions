558b9ed007d52d2784612a5478b4b7affea836cb
Miguel Gomez <mgomez@albany.edu>
2017-08-11 00:22:51 -0400

    Update palin.java

--- LinkedListQuestions/CTCI_2.3/palin.java
+++ LinkedListQuestions/CTCI_2.3/palin.java
@@ -14,26 +14,7 @@ public class palin {
 
 
         Node list = newList(head.next.next.next); //Sending the middle node of 1-2-3-3-4-5-6
-//
-//
-//
-//       Node head = insert(null, two);
-//       insert(head, three);
-//       insert(head, four);
-//        insert(head, eight);
-//        insert(head, five);
-
-
-//        Node one = new Node(1);
-//        Node two = new Node(2);
-//        Node three = new Node(3);
-//        Node five = new Node(5);
-//
-//        Node head = insert(null, five);
-//
-//        insert(head, one);
-//        insert(head, two);
-//        insert(head, three);
+        
         while (head != null){
             System.out.print(head.data);
             head = head.next;
@@ -64,69 +45,7 @@ public class palin {
         temp.next = new Node(data);
     }
 
-
-//    public static Node insert(Node head, Node node) {
-//        if (head == null) {
-//            return node;
-//        }
-//
-//        if (node.getData() <= head.getData()) {
-//            head.setLeftChild(insert(head.getLeftChild(), node));
-//        } else {
-//            head.setRightChild(insert(head.getRightChild(), node));
-//        }
-//
-//        return head;
-//    }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//class Node{
-//    int data;
-//    Node leftChild;
-//    Node rightChild;
-//
-//    Node(int data){
-//        this.data = data;
-//    }
-//
-//    public int getData() {
-//        return data;
-//    }
-//
-//    public Node getLeftChild() {
-//        return leftChild;
-//    }
-//
-//    public void setLeftChild(Node leftChild) {
-//        this.leftChild = leftChild;
-//    }
-//
-//    public Node getRightChild() {
-//        return rightChild;
-//    }
-//
-//    public void setRightChild(Node rightChild) {
-//        this.rightChild = rightChild;
-//    }
-//}
-
-
-
-
-
 
 
 
