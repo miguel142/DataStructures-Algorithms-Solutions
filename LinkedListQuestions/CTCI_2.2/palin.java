758de08f2b80965e3830eab8dc4087fe342a5653
Miguel Gomez <mgomez@albany.edu>
2017-08-10 23:22:19 -0400

    Update palin.java

--- LinkedListQuestions/CTCI_2.2/palin.java
+++ LinkedListQuestions/CTCI_2.2/palin.java
@@ -12,28 +12,6 @@ public class palin {
         addNode(head, 5);
         addNode(head, 6);
 
-
-//
-//
-//
-//       Node head = insert(null, two);
-//       insert(head, three);
-//       insert(head, four);
-//        insert(head, eight);
-//        insert(head, five);
-
-
-//        Node one = new Node(1);
-//        Node two = new Node(2);
-//        Node three = new Node(3);
-//        Node five = new Node(5);
-//
-//        Node head = insert(null, five);
-//
-//        insert(head, one);
-//        insert(head, two);
-//        insert(head, three);
-
         int count = kNode(head, 4);
 
         System.out.print(count);
@@ -69,12 +47,6 @@ public class palin {
     //Answer
 
 
-
-
-
-
-
-
     public static void addNode(Node head, int data){
         Node temp = head;
         while(temp.next != null){
@@ -84,70 +56,7 @@ public class palin {
         temp.next = new Node(data);
     }
 
-
-//    public static Node insert(Node head, Node node) {
-//        if (head == null) {
-//            return node;
-//        }
-//
-//        if (node.getData() <= head.getData()) {
-//            head.setLeftChild(insert(head.getLeftChild(), node));
-//        } else {
-//            head.setRightChild(insert(head.getRightChild(), node));
-//        }
-//
-//        return head;
-//    }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//class Node{
-//    int data;
-//    Node leftChild;
-//    Node rightChild;
-//
-//    Node(int data){
-//        this.data = data;
-//    }
-//
-//    public int getData() {
-//        return data;
-//    }
-//
-//    public Node getLeftChild() {
-//        return leftChild;
-//    }
-//
-//    public void setLeftChild(Node leftChild) {
-//        this.leftChild = leftChild;
-//    }
-//
-//    public Node getRightChild() {
-//        return rightChild;
-//    }
-//
-//    public void setRightChild(Node rightChild) {
-//        this.rightChild = rightChild;
-//    }
-//}
-
-
-
-
-
-
 
 
 
