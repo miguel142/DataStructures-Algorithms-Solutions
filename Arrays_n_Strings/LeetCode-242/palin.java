557752ee76415f8f37a247a44afc51b73492b661
Miguel Gomez <mgomez@albany.edu>
2017-08-16 14:44:21 -0500

    Add files via upload

---
+++ Arrays_n_Strings/LeetCode-242/palin.java
@@ -0,0 +1,170 @@
+
+import java.util.*;
+
+public class palin {
+
+    public static void main(String[] args) {
+
+//        Node head = new Node(1);
+//        addNode(head, 2);
+//        addNode(head, 3);
+//        addNode(head, 3);
+//        addNode(head, 5);
+//        addNode(head, 6);
+
+
+
+//        {1, 7, 5, 50 ,40 ,10} post
+//
+//        int[] array = {1, 7, 5, 50, 40, 10};
+//
+//        Node one = new Node(1);
+//        Node seven = new Node(7);
+//        Node five = new Node(5);
+//        Node fifty = new Node(50);
+//        Node forty = new Node(40);
+//        Node ten = new Node(10);
+//
+//        Node head = insert(null, ten);
+//        insert(head, forty);
+//        insert(head, fifty);
+//        insert(head, five);
+//        insert(head, one);
+//        insert(head, seven);
+
+
+        String word = "the";
+        String word2 = "ege";
+
+        boolean check = list(word, word2);
+
+        System.out.println(check);
+
+
+
+
+
+    }
+
+
+
+    public static boolean list(String string1, String string2){
+
+        char[] str1 = string1.toCharArray();
+        char[] str2 = string2.toCharArray();
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        String realString = String.valueOf(str1);
+        String realString2 = String.valueOf(str2);
+
+
+        if(realString.equals(realString2)){
+            return true;
+        }
+
+        return false;
+
+    }
+
+
+
+
+
+
+
+//    public static Node insert(Node head, Node node) {
+//        if (head == null) {
+//            return node;
+//        }
+//
+//        if (node.data <= head.data) {
+//            head.left = insert(head.left, node);
+//            head.left.parent = head;
+//
+//        } else {
+//            head.right = insert(head.right, node);
+//            head.right.parent = head;
+//        }
+//
+//        return head;
+//    }
+//
+//
+//
+//
+//    public static ArrayList<Integer> inOrder(Node head, ArrayList<Integer> list){
+//        if(head == null){
+//            return list;
+//        }
+//
+//        inOrder(head.left, list);
+//        list.add(head.data);
+//        inOrder(head.right, list);
+//
+//
+//        return list;
+//    }
+
+
+
+
+
+
+
+//    public static class Node{
+//        Node next = null;
+//        int data;
+//
+//        Node(int data){
+//            this.data = data;
+//            this.next = next;
+//        }
+//    }
+//
+//
+//    public static void addNode(Node head, int data){
+//        Node temp = head;
+//        while(temp.next != null){
+//            temp = temp.next;
+//        }
+//
+//        temp.next = new Node(data);
+//    }
+
+
+
+}
+
+
+
+
+
+
+//
+//class Node{
+//    int data;
+//    Node left;
+//    Node right;
+//    Node parent;
+//
+//    Node(int data){
+//        this.data = data;
+//        left = right = parent = null;
+//    }
+//
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
