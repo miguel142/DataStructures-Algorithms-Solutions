c33c997a23d51bc00a5aab99b86a16273a286c0e
Miguel Gomez <mgomez@albany.edu>
2017-08-16 21:53:56 -0500

    Add files via upload

---
+++ Arrays_n_Strings/LeetCode-283/palin.java
@@ -0,0 +1,175 @@
+
+import java.util.*;
+
+public class palin {
+
+    public static void main(String[] args) {
+//
+//        Node head = new Node(1);
+//        addNode(head, 2);
+//        addNode(head, 3);
+//        addNode(head, 3);
+//        addNode(head, 5);
+//        addNode(head, 6);
+//
+
+
+//        {1, 7, 5, 50 ,40 ,10} post
+//
+//        int[] array = {1, 7, 5, 50, 40, 10};
+//
+//        Node one = new Node(1);
+//        Node seven = new Node(7);
+//        Node five = new Node(5);
+//        Node fifty = new Node(50);
+//        Node forty = new Node(40);
+//        Node ten = new Node(10);
+//
+//        Node head = insert(null, ten);
+//        insert(head, forty);
+//        insert(head, fifty);
+//        insert(head, five);
+//        insert(head, one);
+//        insert(head, seven);
+
+        int[] array = {0, 0, 2, 0, 12, 0};
+
+        array = list(array);
+
+        for(int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+
+
+    }
+
+
+
+    public static int[] list(int[] array){
+        int i = 0;
+        int j= 0;
+
+        while(i < array.length){
+            if(array[i] == 0){
+
+                while(array[j] == 0 && j < array.length - 1){
+                    ++j;
+                }
+
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+
+
+            }
+
+
+            ++i;
+            j = i;
+        }
+
+
+        return array;
+    }
+
+
+
+
+
+
+
+//    public static Node insert(Node head, Node node) {
+//        if (head == null) {
+//            return node;
+//        }
+//
+//        if (node.data <= head.data) {
+//            head.left = insert(head.left, node);
+//            head.left.parent = head;
+//
+//        } else {
+//            head.right = insert(head.right, node);
+//            head.right.parent = head;
+//        }
+//
+//        return head;
+//    }
+//
+//
+//
+//
+//    public static ArrayList<Integer> inOrder(Node head, ArrayList<Integer> list){
+//        if(head == null){
+//            return list;
+//        }
+//
+//        inOrder(head.left, list);
+//        list.add(head.data);
+//        inOrder(head.right, list);
+//
+//
+//        return list;
+//    }
+
+
+
+
+
+
+
+//    public static class Node{
+//        Node next = null;
+//        int data;
+//
+//        Node(int data){
+//            this.data = data;
+//            this.next = next;
+//        }
+//    }
+//
+//
+//    public static void addNode(Node head, int data){
+//        Node temp = head;
+//        while(temp.next != null){
+//            temp = temp.next;
+//        }
+//
+//        temp.next = new Node(data);
+//    }
+
+
+
+}
+
+
+
+
+
+
+//
+//class Node{
+//    int data;
+//    Node left;
+//    Node right;
+//    Node parent;
+//
+//    Node(int data){
+//        this.data = data;
+//        left = right = parent = null;
+//    }
+//
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
