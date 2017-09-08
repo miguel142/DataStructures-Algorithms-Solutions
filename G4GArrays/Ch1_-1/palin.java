182de3a64e0dc36e5763f965d4fe8bd440ff01f4
Miguel Gomez <mgomez@albany.edu>
2017-09-08 13:59:33 -0400

    Update palin.java

--- G4GArrays/Ch1_-1/palin.java
+++ G4GArrays/Ch1_-1/palin.java
@@ -4,34 +4,6 @@ import java.util.*;
 public class palin {
 
     public static void main(String[] args) {
-//
-//        Node head = new Node(1);
-//        addNode(head, 2);
-//        addNode(head, 3);
-//        addNode(head, 3);
-//        addNode(head, 5);
-//        addNode(head, 6);
-//
-
-
-//        {1, 7, 5, 50 ,40 ,10} post
-//
-//        int[] array = {1, 7, 5, 50, 40, 10};
-//
-//        Node one = new Node(1);
-//        Node seven = new Node(7);
-//        Node five = new Node(5);
-//        Node fifty = new Node(50);
-//        Node forty = new Node(40);
-//        Node ten = new Node(10);
-//
-//        Node head = insert(null, ten);
-//        insert(head, forty);
-//        insert(head, fifty);
-//        insert(head, five);
-//        insert(head, one);
-//        insert(head, seven);
-
 
        int[] array = {1, 2, 3, 5, 8};
 
@@ -77,98 +49,7 @@ public class palin {
     }
 
 
-
-
-
-
-
-//    public static Node insert(Node head, Node node) {
-//        if (head == null) {
-//            return node;
-//        }
-//
-//        if (node.data <= head.data) {
-//            head.left = insert(head.left, node);
-//            head.left.parent = head;
-//
-//        } else {
-//            head.right = insert(head.right, node);
-//            head.right.parent = head;
-//        }
-//
-//        return head;
-//    }
-//
-//
-//
-//
-//    public static ArrayList<Integer> inOrder(Node head, ArrayList<Integer> list){
-//        if(head == null){
-//            return list;
-//        }
-//
-//        inOrder(head.left, list);
-//        list.add(head.data);
-//        inOrder(head.right, list);
-//
-//
-//        return list;
-//    }
-
-
-
-
-
-
-
-//    public static class Node{
-//        Node next = null;
-//        int data;
-//
-//        Node(int data){
-//            this.data = data;
-//            this.next = next;
-//        }
-//    }
-//
-//
-//    public static void addNode(Node head, int data){
-//        Node temp = head;
-//        while(temp.next != null){
-//            temp = temp.next;
-//        }
-//
-//        temp.next = new Node(data);
-//    }
-
-
-
 }
-
-
-
-
-
-
-//
-//class Node{
-//    int data;
-//    Node left;
-//    Node right;
-//    Node parent;
-//
-//    Node(int data){
-//        this.data = data;
-//        left = right = parent = null;
-//    }
-//
-//
-//}
-
-
-
-
-
 
 
 
