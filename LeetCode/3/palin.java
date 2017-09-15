0a3b582b2a35ae35ec99b362f2b4ec54b2782543
Miguel Gomez <mgomez@albany.edu>
2017-09-14 22:07:31 -0500

    Add files via upload

---
+++ LeetCode/3/palin.java
@@ -0,0 +1,234 @@
+
+import java.util.*;
+
+public class palin {
+
+    public static void main(String[] args) {
+//
+//        Node head = new Node(1);
+//        addNode(head, 2);
+//        addNode(head, 3);
+//        addNode(head, 3);
+//        addNode(head, 5);
+//        addNode(head, 6);
+//
+
+
+//        {1, 7, 5, 50 ,40 ,10} post
+//
+//        int[] array = {1, 7, 5, 50, 40, 10};
+//
+//        Node one = new Node(1);
+//        Node seven = new Node(7);
+//        Node five = new Node(5);
+//        Node fifty = new Node(50);
+//        Node forty = new Node(40);
+//        Node ten = new Node(10);
+//
+//        Node head = insert(null, ten);
+//        insert(head, forty);
+//        insert(head, fifty);
+//        insert(head, five);
+//        insert(head, one);
+//        insert(head, seven);
+
+//        Node head = new Node(2);
+//        Node one = new Node(1);
+//        Node two = new Node(2);
+//        Node three = new Node(3);
+//        Node four = new Node(4);
+//        Node five = new Node(5);
+//        Node six = new Node(6);
+//
+//        insert(head, one);
+//        insert(head, two);
+//        insert(head, three);
+//        insert(head, four);
+//        insert(head, five);
+//        insert(head, six);
+//
+//        Node head1 = new Node(2);
+//        Node one1 = new Node(1);
+//        Node two1 = new Node(2);
+//        Node three1 = new Node(3);
+//        Node four1 = new Node(4);
+//        Node five1 = new Node(5);
+//        Node six1 = new Node(6);
+//
+//        insert(head1, one1);
+//        insert(head1, two1);
+//        insert(head1, three1);
+//        insert(head1, four1);
+//        insert(head1, five1);
+//        insert(head1, six1);
+//
+
+        String longest = "yesty";
+        int temp = longSub(longest);
+
+
+        //longest = longSub(longest);
+
+        System.out.println(temp);
+
+    }
+
+
+    //Answer
+
+    public static int longSub(String str){
+        char[] array = str.toCharArray();
+        String maxSub = "";
+        int max = 0;
+
+
+        for(int i = 0; i < array.length; i++){
+
+            int temp = 0;
+            HashSet<Character> set = new HashSet<Character>();
+
+
+            for(int j = i; j < array.length; j++){
+
+                if(!set.contains(array[j]) && j != array.length - 1){
+                    set.add(array[j]);
+                    ++temp;
+                }
+                else{
+                    if(!set.contains(array[j])){
+                        ++temp;
+
+                        if(temp > max){
+                            max = temp;
+                        }
+                        break;
+
+                    }
+
+                    else{
+                        if(temp > max){
+                            max = temp;
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        return max;
+
+    }
+
+
+
+
+
+    
+
+
+
+
+
+//    public static Node insert(Node head, Node node) {
+//        if (head == null) {
+//            return node;
+//        }
+//
+//        if (node.data <= head.data) {
+//            head.left = insert(head.left, node);
+//            head.left.parent = head;
+//
+//        } else {
+//            head.right = insert(head.right, node);
+//            head.right.parent = head;
+//        }
+//
+//        return head;
+//    }
+
+//
+//
+//
+//    public static ArrayList<Integer> inOrder(Node head, ArrayList<Integer> list){
+//        if(head == null){
+//            return list;
+//        }
+//
+//        inOrder(head.left, list);
+//        list.add(head.data);
+//        inOrder(head.right, list);
+//
+//
+//        return list;
+//    }
+
+
+
+
+
+//    public static void printL(Node head){
+//        Node temp = head;
+//
+//        while(temp != null){
+//            System.out.println(temp.data);
+//            temp = temp.next;
+//        }
+//    }
+
+
+//    public static class Node{
+//        Node next = null;
+//        int data;
+//
+//        Node(int data){
+//            this.data = data;
+//            this.next = next;
+//        }
+//    }
+//
+//
+//    public static void addNode(Node head, int data){
+//        Node temp = head;
+//        while(temp.next != null){
+//            temp = temp.next;
+//        }
+//
+//        temp.next = new Node(data);
+//    }
+
+
+
+}
+
+
+
+
+
+
+//
+//class Node{
+//    int data;
+//    Node left;
+//    Node right;
+//    Node parent;
+//
+//    Node(int data){
+//        this.data = data;
+//        left = right = parent = null;
+//    }
+//
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
