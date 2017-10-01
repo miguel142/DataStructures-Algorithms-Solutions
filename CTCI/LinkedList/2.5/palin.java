523660b70796bef405e5071a725c42b1224ef19e
Miguel Gomez <mgomez@albany.edu>
2017-10-01 14:53:11 -0400

    Update palin.java

--- CTCI/LinkedList/2.5/palin.java
+++ CTCI/LinkedList/2.5/palin.java
@@ -18,73 +18,6 @@ public class palin {
 
 
 
-//        {1, 7, 5, 50 ,40 ,10} post
-//
-//        int[] array = {1, 7, 5, 50, 40, 10};
-//
-//        Node one = new Node(1);
-//        Node seven = new Node(7);
-//        Node five = new Node(5);
-//        Node fifty = new Node(50);
-//        Node forty = new Node(40);
-//        Node ten = new Node(10);
-//
-//        Node head = insert(null, ten);
-//        insert(head, forty);
-//        insert(head, fifty);
-//        insert(head, five);
-//        insert(head, one);
-//        insert(head, seven);
-
-//        Node head = new Node(2);
-//        Node one = new Node(1);
-//        Node two = new Node(2);
-//        Node three = new Node(3);
-//        Node four = new Node(4);
-//        Node five = new Node(5);
-//        Node six = new Node(6);
-//
-//        insert(head, one);
-//        insert(head, two);
-//        insert(head, three);
-//        insert(head, four);
-//        insert(head, five);
-//        insert(head, six);
-//
-//        Node head1 = new Node(2);
-//        Node one1 = new Node(1);
-//        Node two1 = new Node(2);
-//        Node three1 = new Node(3);
-//        Node four1 = new Node(4);
-//        Node five1 = new Node(5);
-//        Node six1 = new Node(6);
-//
-//        insert(head1, one1);
-//        insert(head1, two1);
-//        insert(head1, three1);
-//        insert(head1, four1);
-//        insert(head1, five1);
-//        insert(head1, six1);
-//
-
-//        Node head = new Node(1);
-//        Node head2 = new Node(2);
-//
-//        addNode(head, 2);
-//        addNode(head, 3);
-//        addNode(head,6);
-//        addNode(head,7);
-//        addNode(head,8);
-//        addNode(head, 9);
-//        addNode(head, 9);
-//
-//        addNode(head2,6);
-//        addNode(head2,7);
-//        addNode(head2,8);
-                            //     j
-
-
-
     }
 
 
@@ -123,42 +56,6 @@ public class palin {
 
     }
 
-
-
-
-
-//    public static Node insert(Node head, Node node) {
-//        if (head == null) {
-//            return node;
-//        }
-//
-//        if (node.data <= head.data) {
-//            head.left = insert(head.left, node);
-//            head.left.parent = head;
-//
-//        } else {
-//            head.right = insert(head.right, node);
-//            head.right.parent = head;
-//        }
-//
-//        return head;
-//    }
-
-//
-//
-//
-//    public static ArrayList<Integer> inOrder(Node head, ArrayList<Integer> list){
-//        if(head == null){
-//            return list;
-//        }
-//
-//        inOrder(head.left, list);
-//        list.add(head.data);
-//        inOrder(head.right, list);
-//
-//
-//        return list;
-//    }
 
 
     public static void printL(Node head) {
