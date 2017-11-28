0f8e9f2dbcaca829599fec5111164d3bfdc096ec
Miguel Gomez <mgomez@albany.edu>
2017-11-28 15:38:45 -0500

    Add files via upload

--- palin.java
+++ palin.java
@@ -18,36 +18,7 @@ public class palin {
 //        {1, 7, 5, 50 ,40 ,10} post
 //
 //        int[] array = {1, 7, 5, 50, 40, 10};
-//
-//        Node head = new Node(2);
-//        Node one = new Node(1);
-//        Node two = new Node(2);
-//        Node three = new Node(3);
-//        Node four = new Node(4);
-//        Node five = new Node(5);
-//        Node six = new Node(6);
-//
-//        insert(head, one);
-//        insert(head, two);
-//        insert(head, three);
-//        insert(head, four);
-//        insert(head, five);
-//        insert(head, six);
-//
-//        Node head1 = new Node(2);
-//        Node one1 = new Node(1);
-//        Node two1 = new Node(2);
-//        Node three1 = new Node(3);
-//        Node four1 = new Node(4);
-//        Node five1 = new Node(5);
-//        Node six1 = new Node(6);
-//
-//        insert(head1, one1);
-//        insert(head1, two1);
-//        insert(head1, three1);
-//        insert(head1, four1);
-//        insert(head1, five1);
-//        insert(head1, six1);
+
 //
 
 //        Node head = new Node(1);
@@ -66,111 +37,156 @@ public class palin {
 //        addNode(head2,8);
         //     j
 
-//        Node one = new Node(1);
-//        Node seven = new Node(7);
-//        Node five = new Node(5);
-//        Node fifty = new Node(50);
-//        Node forty = new Node(40);
-//        Node ten = new Node(10);
 //
-//        Node head = insert(null, ten);
-//        insert(head, forty);
-//        insert(head, fifty);
-//        insert(head, five);
-//        insert(head, one);
-//        insert(head, seven);
 
-        System.out.println(subAtromic("abcd", "cdabcdabcda"));
+//
+//        Node head1 = new Node(2);
+//        Node one1 = new Node(3);
+//        Node two1 = new Node(4);
+//        Node three1 = new Node(3);
+//        Node four1 = new Node(4);
+//        Node five1 = new Node(5);
+//        Node six1 = new Node(6);
+//
+//        insert(head1, one1);
+//        insert(head1, two1);
+//        insert(head1, three1);
+//        insert(head1, four1);
+//        insert(head1, five1);
+//        insert(head1, six1);
+
+//        Node head1 = new Node(20);
+//        Node one1 = new Node(8);
+//        Node two1 = new Node(4);
+//        Node three1 = new Node(12);
+//        Node four1 = new Node(10);
+//        Node five1 = new Node(14);
+//        Node six1 = new Node(22);
+//
+//        insert(head1, one1);
+//        insert(head1, two1);
+//        insert(head1, three1);
+//        insert(head1, four1);
+//        insert(head1, five1);
+//        insert(head1, six1);
+
+
+        //          //          //
+        //String[] results = {"1, 234", "2, 345", "2, 645", "2, 2445","1, 8943", "5, 234", "3, 234", "8, 456", "9, 645", "10, 345", "11, 345"};
+
+
+//        Node head = new Node(2);
+//        Node one = new Node(3);
+//        Node two = new Node(4);
+//        Node three = new Node(3);
+//        Node four = new Node(4);
+//        Node five = new Node(5);
+//        Node six = new Node(6);
+//
+//        insert(head, one);
+//        insert(head, two);
+//        insert(head, three);
+//        insert(head, four);
+//        insert(head, five);
+//        insert(head, six);
+
+
+
+        Node one = new Node(1);
+        Node seven = new Node(7);
+        Node five = new Node(5);
+        Node fifty = new Node(50);
+        Node forty = new Node(40);
+        Node ten = new Node(10);
+
+        Node head = insert(null, ten);
+        insert(head, forty);
+        insert(head, fifty);
+        insert(head, five);
+        insert(head, one);
+        insert(head, seven);
+
+        inOrder(head);
+
+        System.out.println(check(head));
+
 
     }
 
+    public static boolean check(Node head){
 
-    public static int subAtromic(String A, String B){
+        ArrayList<Integer> list = new ArrayList<>();
 
-        if(A == null || B == null){
-            System.out.println("Invalid");
-            return -1;
-        }
+        createList(head, list);
 
-        int count = 0;
+        for(int i = 0; i < list.size() -1; i++){
 
-        if(B.charAt(0) != 'a'){
-            ++count;
-        }
-
-        for(int i  = 0; i < B.length(); i++){
-
-            if(B.charAt(i) == 'a'){
-                for(int j = i; j < B.length(); j++){
-                    if(B.charAt(j) == 'd'){
-                        ++count;
-                        i = j;
-                        break;
-                    }
-                }
+            if(list.get(i) > list.get(i + 1)){
+                return false;
             }
         }
 
-        if(B.charAt(B.length() - 1) != 'd'){
-            ++count;
+        return true;
+
+    }
+
+    public static void createList(Node node, ArrayList<Integer> list){
+
+        if(node == null){
+            return;
         }
 
-        if(B.charAt(0) == 'a' && B.charAt(B.length() - 1) == 'd'){
-            ++count;
-        }
+        createList(node.left, list);
+        list.add(node.data);
+        createList(node.right, list);
 
-        StringBuilder build = new StringBuilder();
+        return;
 
-        for(int i = 0; i < count; i++){
-            build.append(A);
-        }
-
-        String newStr = build.toString();
-
-        if(!newStr.contains(B)){
-            return -1;
-        }
-
-        return count;
     }
 
 
 
-//    public static Node insert(Node head, Node node) {
-//        if (head == null) {
-//            return node;
-//        }
-//
-//        if (node.data <= head.data) {
-//            head.left = insert(head.left, node);
-//            head.left.parent = head;
-//
-//        } else {
-//            head.right = insert(head.right, node);
-//            head.right.parent = head;
-//        }
-//
-//        return head;
-//    }
-//
-//
-//
-//
-//    public static ArrayList<Integer> inOrder(Node head, ArrayList<Integer> list) {
-//        if (head == null) {
-//            return list;
-//        }
-//
-//        inOrder(head.left, list);
-//        list.add(head.data);
-//        inOrder(head.right, list);
-//
-//
-//        return list;
-//    }
-//    }
 
 
+
+
+
+
+
+
+    public static Node insert(Node head, Node node) {
+
+        if (head == null) {
+            return node;
+        }
+
+        if (node.data <= head.data) {
+            head.left = insert(head.left, node);
+            //head.left.parent = head;
+
+        } else {
+            head.right = insert(head.right, node);
+            //head.right.parent = head;
+        }
+
+        return head;
+    }
+
+
+    public static void inOrder(Node head) {
+        if (head == null) {
+            return;
+        }
+
+        inOrder(head.left);
+        System.out.println(head.data);
+        inOrder(head.right);
+
+
+        return;
+    }
+
+//
 //    public static void printL(Node head) {
 //        Node temp = head;
 //
@@ -203,26 +219,47 @@ public class palin {
 //
 //
 //}
+//
+//
+//
 
 }
 
 
 
 
-//
+
+
+
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+        left = right = null;
+    }
+
+
+}
+
 //class Node{
-//    int data;
-//    Node left;
-//    Node right;
-//    Node parent;
+//    int label;
+//    LinkedList<Node> childs = new LinkedList<>();
 //
-//    Node(int data){
-//        this.data = data;
-//        left = right = parent = null;
+//     Node(int data){
+//        this.label = data;
+//        childs = new LinkedList<Node>();
 //    }
 //
+//    public Node getNode(int source, int edge){
+//
+//    }
 //
 //}
+
 
 
 
